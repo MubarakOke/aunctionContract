9a73957339c5ec801c5cc80945c8b8766fb31d9c
mubarakOke <okeolamubarak18@gmail.com>
2024-03-24 12:13:11 +0100

    auction setup

---
+++ contracts/AuctionNFT.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+
+contract AuctionNFT is ERC721URIStorage {
+    using Strings for uint256;
+    uint currentTokenId;
+
+    mapping(uint256 => uint256) public tokenIdToItem;
+
+    constructor() ERC721("Auncti", "AUN") {}
+
+    function getItems(uint256 tokenId) public view returns (string memory) {
+        uint256 levels = tokenIdToItem[tokenId];
+        return levels.toString();
+    }
+
+    function generateItem(
+        uint256 tokenId
+    ) public view returns (string memory) {
+        bytes memory svg = abi.encodePacked(
+            '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
+            "<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>",
+            '<rect width="100%" height="100%" fill="black" />',
+            '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Warrior",
+            "</text>",
+            '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Levels: ",
+            getItems(tokenId),
+            "</text>",
+            "</svg>"
+        );
+        return
+            string(
+                abi.encodePacked(
+                    "data:image/svg+xml;base64,",
+                    Base64.encode(svg)
+                )
+            );
+    }
+
+    function getTokenURI(uint256 tokenId) public view returns (string memory) {
+        bytes memory dataURI = abi.encodePacked(
+            "{",
+            '"name": "Chain Battles #',
+            tokenId.toString(),
+            '",',
+            '"description": "Battles on chain",',
+            '"image": "',
+            generateItem(tokenId),
+            '"',
+            "}"
+        );
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(dataURI)
+                )
+            );
+    }
+
+    function mint() public {
+        currentTokenId += 1;
+        _mint(msg.sender, currentTokenId);
+        tokenIdToItem[currentTokenId] = 0;
+        _setTokenURI(currentTokenId, getTokenURI(currentTokenId));
+    }
+}
---
+++ test/deployDiamond.t.sol
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "../contracts/interfaces/IDiamondCut.sol";
+import "../contracts/facets/DiamondCutFacet.sol";
+import "../contracts/facets/DiamondLoupeFacet.sol";
+import "../contracts/facets/OwnershipFacet.sol";
+
+import "../contracts/facets/ERC20Facet.sol";
+import "../contracts/facets/AuctionBidFacet.sol";
+
+import "../contracts/AuctionNFT.sol";
+import "forge-std/Test.sol";
+import "../contracts/Diamond.sol";
+
+import "../contracts/libraries/LibAppStorage.sol";
+
+contract DiamondDeployer is Test, IDiamondCut {
+    //contract types of facets to be deployed
+    // LibAppStorage.Layout internal l;
+
+    Diamond diamond;
+    DiamondCutFacet dCutFacet;
+    DiamondLoupeFacet dLoupe;
+    OwnershipFacet ownerF;
+    ERC20Facet erc20Facet;
+    AuctionBidFacet aFacet;
+    AuctionNFT erc721Token;
+
+    address A = address(0xa);
+    address B = address(0xb);
+    address C = address(0xc);
+
+    AuctionBidFacet boundAuction;
+    ERC20Facet boundERC;
+
+    function setUp() public {
+        //deploy facets
+        dCutFacet = new DiamondCutFacet();
+        diamond = new Diamond(address(this), address(dCutFacet));
+        dLoupe = new DiamondLoupeFacet();
+        ownerF = new OwnershipFacet();
+        erc20Facet = new ERC20Facet();
+        aFacet = new AuctionBidFacet();
+        erc721Token = new AuctionNFT();
+
+        //upgrade diamond with facets
+
+        //build cut struct
+        FacetCut[] memory cut = new FacetCut[](4);
+
+        cut[0] = (
+            FacetCut({
+                facetAddress: address(dLoupe),
+                action: FacetCutAction.Add,
+                functionSelectors: generateSelectors("DiamondLoupeFacet")
+            })
+        );
+
+        cut[1] = (
+            FacetCut({
+                facetAddress: address(ownerF),
+                action: FacetCutAction.Add,
+                functionSelectors: generateSelectors("OwnershipFacet")
+            })
+        );
+        cut[2] = (
+            FacetCut({
+                facetAddress: address(aFacet),
+                action: FacetCutAction.Add,
+                functionSelectors: generateSelectors("AuctionBidFacet")
+            })
+        );
+
+        cut[3] = (
+            FacetCut({
+                facetAddress: address(erc20Facet),
+                action: FacetCutAction.Add,
+                functionSelectors: generateSelectors("ERC20Facet")
+            })
+        );
+
+        //upgrade diamond
+        IDiamondCut(address(diamond)).diamondCut(cut, address(0x0), "");
+
+        A = mkaddr("staker a");
+        B = mkaddr("staker b");
+        C = mkaddr("staker c");
+
+        //mint test tokens
+        ERC20Facet(address(diamond)).mintTo(A);
+        ERC20Facet(address(diamond)).mintTo(B);
+
+        boundAuction = AuctionBidFacet(address(diamond));
+        boundERC = ERC20Facet(address(diamond));
+    }
+
+
+
+    function generateSelectors(
+        string memory _facetName
+    ) internal returns (bytes4[] memory selectors) {
+        string[] memory cmd = new string[](3);
+        cmd[0] = "node";
+        cmd[1] = "scripts/genSelectors.js";
+        cmd[2] = _facetName;
+        bytes memory res = vm.ffi(cmd);
+        selectors = abi.decode(res, (bytes4[]));
+    }
+
+    function mkaddr(string memory name) public returns (address) {
+        address addr = address(
+            uint160(uint256(keccak256(abi.encodePacked(name))))
+        );
+        vm.label(addr, name);
+        return addr;
+    }
+
+    function switchSigner(address _newSigner) public {
+        address foundrySigner = 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
+        if (msg.sender == foundrySigner) {
+            vm.startPrank(_newSigner);
+        } else {
+            vm.stopPrank();
+            vm.startPrank(_newSigner);
+        }
+    }
+
+    function diamondCut(
+        FacetCut[] calldata _diamondCut,
+        address _init,
+        bytes calldata _calldata
+    ) external override {}
+}
